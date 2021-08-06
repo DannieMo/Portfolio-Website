a3af2a989fe6fa6baed045aa817a31c4aaf23023
mprotic123 <milan.protic@ts9maj.edu.rs>
2021-08-06 19:32:16 +0200

    Add files to local storage

--- JS/localstorage.js
+++ JS/localstorage.js
@@ -14,3 +14,15 @@ const storeLocally = () => {
   localStorage.setItem('formDataObject', stringifiedData);
 };
 
+const formDataObject = localStorage.getItem('formDataObject');
+if (formDataObject) {
+  const refinedFormDataObject = JSON.parse(formDataObject);
+  document.querySelector('#full-name').value = refinedFormDataObject.name;
+  document.querySelector('#email').value = refinedFormDataObject.email;
+  document.querySelector('#message').value = refinedFormDataObject.message;
+}
+const allFields = Array.from(document.querySelectorAll('.store'));
+allFields.map((field) => {
+  field.addEventListener('keyup', storeLocally);
+  return true;
+});
