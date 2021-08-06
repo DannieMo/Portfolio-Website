8e3a0e0b74227e73885c16f79cc86c7ca6326ae1
DannieMo <53879944+DannieMo@users.noreply.github.com>
2021-08-06 19:40:15 +0100

    Update validation.js

--- JS/validation.js
+++ JS/validation.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('form');
-const form = document.querySelector('form');
 const email = document.getElementById('email');
 const alertMsg = document.querySelector('.alert_msg');
 
