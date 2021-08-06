ea860b9fd61cded95c11979cf8a2555ec04042af
DannieMo <danielowiri4zoe@gmail.com>
2021-08-06 16:42:52 +0100

    Edit validation form codes

--- JS/validation.js
+++ JS/validation.js
@@ -1,24 +1,27 @@
+const form = document.querySelector('form');
 const email = document.getElementById('email');
 const alertMsg = document.querySelector('.alert_msg');
-const button = document.querySelector('button');
 
 email.addEventListener('focus', () => {
   email.style.outline = 'none';
 });
 
-button.addEventListener('click', (e) => {
-  e.preventDefault();
+alertMsg.textContent = 'Please make sure the email is all lower case';
+alertMsg.style.color = 'rgba(255, 97, 99, 1)';
+
+form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
-    alertMsg.style.display = 'block';
-    alertMsg.style.color = 'rgba(255, 97, 99, 1)';
-    alertMsg.textContent = 'Please make sure the email is all lower case';
+    alertMsg.style.display = 'inline-block';
     email.style.outline = 'solid rgba(255, 97, 99, 1)';
+    e.preventDefault();
     alertMsg.classList.toggle('fade');
-    setTimeout(() => {
-      alertMsg.classList.toggle('fade');
-      alertMsg.style.display = 'none';
-    }, 7000);
   } else {
-    form.submit();
+    alertMsg.style.display = 'none';
   }
+  setTimeout(() => {
+    alertMsg.classList.toggle('fade');
+  }, 5000);
+  setTimeout(() => {
+    alertMsg.style.display = 'none';
+  }, 7000);
 });
